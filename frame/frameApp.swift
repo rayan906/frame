6d914f9457229da67c8a3eb72d73beb9e500920e
rayan906 <krayanahmed2004@gmail.com>
2026-02-13 17:46:01 +0530

    Initial Commit

---
+++ frame/frameApp.swift
@@ -0,0 +1,17 @@
+//
+//  frameApp.swift
+//  frame
+//
+//  Created by apprikart on 13/02/26.
+//
+
+import SwiftUI
+
+@main
+struct frameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
